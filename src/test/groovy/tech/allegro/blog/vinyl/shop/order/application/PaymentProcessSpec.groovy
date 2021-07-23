44b6be43f72321341d5ed2f300f4f1a5d3208530
Kamil Jedrzejuk <kamil.jedrzejuk@allegro.pl>
2021-07-23 11:50:02 +0200

    Added additional SourceSet for not-refactored unit tests.

---
+++ src/test/groovy/tech/allegro/blog/vinyl/shop/order/application/PaymentProcessSpec.groovy
@@ -0,0 +1,13 @@
+package tech.allegro.blog.vinyl.shop.order.application
+
+import spock.lang.Specification
+
+class PaymentProcessSpec extends Specification {
+
+
+  def "b"() {
+
+    expect:
+        1 == 1
+  }
+}
